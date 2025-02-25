9831a2b55e02538f685dffe9ab7e0062cec13287
MAOZ <maooz4426@gmail.com>
2025-02-25 16:44:57 +0900

    fix

--- Pandator/Assets/Connection/Scripts/ConnectionManager.cs
+++ Pandator/Assets/Connection/Scripts/ConnectionManager.cs
@@ -7,20 +7,25 @@ public class ConnectionManager : MonoBehaviourPunCallbacks
     private string roomName = "Pandator";
     void Start()
     {
+        //接続
         PhotonNetwork.ConnectUsingSettings();
     }
     
-
+    //マスターサーバーへ接続できたら呼ばれる
     public override void OnConnectedToMaster()
     {
+        //Pandatorルームへの入室
         PhotonNetwork.JoinRoom(roomName);
     }
 
+    //ルームに入室されたら呼ばれる
     public override void OnJoinedRoom()
     {
         Debug.Log("OnJoinedRoom");
     }
-
+    
+    // ルーム入室に失敗されたら呼び出される
+    // ルーム入室に失敗したら部屋ができていない可能性が高いので部屋を作るコードを書いている。
     public override void OnJoinRoomFailed(short returnCode, string message) {
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 4;
