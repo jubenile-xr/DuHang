54bce20a81fbee31195acfc38674a3120f951ec6
Kumazawa <is0667fk@ed.ritsumei.ac.jp>
2025-02-26 18:26:24 -0800

    NetGunの設定

---
+++ Pandator/Assets/NetGun.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NetGun : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("GunComponent")]
+    [SerializeField] private GameObject BulletType;
+    [SerializeField] private float BulletSpeed = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shot() //弾の発射
+    {
+        //弾の発射位置(transform.position)は再考の余地あり
+        GameObject bulletInstance = Instantiate(BulletType, transform.position, Quaternion.LookRotation(transform.forward));
+        bulletInstance.GetComponent<Rigidbody>().AddForce(transform.forward * BulletSpeed);
+    }
+}
