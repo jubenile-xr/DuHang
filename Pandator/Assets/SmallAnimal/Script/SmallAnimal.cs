ac9eacb58fe04bb5a1ba2076ca66adca39b17cf2
bearl <125728897+bearl27@users.noreply.github.com>
2025-02-28 18:06:37 +0900

    add smallanimal

---
+++ Pandator/Assets/SmallAnimal/Script/SmallAnimal.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SmallAnimal : MonoBehaviour
+{
+    protected string name;
+    protected float moveSpeed;
+    protected GameObject model;
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+}
