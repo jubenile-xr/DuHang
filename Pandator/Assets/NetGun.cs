ace7699583293c4107c373bcf1cca51b090f6d39
Kumazawa <is0667fk@ed.ritsumei.ac.jp>
2025-03-02 14:50:17 -0800

    弾発射メソッドと移動メソッド(仮)

--- Pandator/Assets/NetGun.cs
+++ Pandator/Assets/NetGun.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Oculus.Interaction.Unity.Input;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -8,26 +10,41 @@ public class NetGun : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [Header("GunComponent")]
     [SerializeField] private GameObject BulletType;
-    [SerializeField] private float BulletSpeed = 10f;
+    [SerializeField] private float BulletSpeed = 1;
+    [SerializeField] private GameObject RightController;
+    private float time = 0f;
+    private Boolean shotable = true;
     void Start()
     {
-        
+        Debug.Log("start");
     }
 
     // Update is called once per frame
     void Update()
     {
         //実験用
-        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        if (OVRInput.GetDown(OVRInput.RawButton.A) || Input.GetKeyDown(KeyCode.A))
         {
-            Shot();
+            if (shotable)
+            {
+                Shot();
+                shotable = false;
+            }
+            
+        }
+        time += Time.deltaTime;
+        if (time > 5.0f)
+        {
+            shotable = true;
+            time = 0;
         }
     }
 
     public void Shot() //弾の発射
     {
         //弾の発射位置(transform.position)は再考の余地あり
-        GameObject bulletInstance = Instantiate(BulletType, transform.position, Quaternion.LookRotation(transform.forward));
-        bulletInstance.GetComponent<Rigidbody>().AddForce(transform.forward * BulletSpeed);
+        GameObject bulletInstance = Instantiate(BulletType, RightController.transform.position, Quaternion.LookRotation(RightController.transform.forward));
+        bulletInstance.GetComponent<Rigidbody>().AddForce(RightController.transform.forward * 10 * Time.deltaTime * 1000 * BulletSpeed);
+        Debug.Log("shot!");
     }
 }
