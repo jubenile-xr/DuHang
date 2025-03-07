35c394f2f83dd44dce46c264f8f45cb4b2c9adeb
MAOZ <maooz4426@gmail.com>
2025-03-07 18:22:22 +0900

    animationをprefabに一旦対応

--- Pandator/Assets/Bird/Scripts/Bird.cs
+++ Pandator/Assets/Bird/Scripts/Bird.cs
@@ -6,12 +6,24 @@ public class Bird : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
     private GameObject model;
+    
+    private Animator animator;
     private void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
     private void Update()
     {
-
+        animator.SetInteger("moving",0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetInteger("moving",1);
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetInteger("moving",2);
+        }else if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetInteger("moving",3);
+        }
     }
 }
