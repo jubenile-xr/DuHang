95085abbc21e642caf8e8123e496f7ac383c48a4
MAOZ <maooz4426@gmail.com>
2025-03-21 09:06:36 +0900

    fix

--- Pandator/Assets/Player/Scripts/PhotonGameManager.cs
+++ Pandator/Assets/Player/Scripts/PhotonGameManager.cs
@@ -10,7 +10,8 @@ public class PhotonGameManager : MonoBehaviourPunCallbacks
     [SerializeField] public bool IsBird;
     [SerializeField] public bool IsMouse;
     [SerializeField] public bool IsPanda;
-    [SerializeField] public GameObject gameManager;
+    // [SerializeField] public GameObject gameManager;
+    private GameManager gameManager;
 
     private GameObject player;
     void Start()
@@ -37,27 +38,32 @@ public class PhotonGameManager : MonoBehaviourPunCallbacks
     // ルーム参加に成功した時の処理
     public override void OnJoinedRoom()
     {
+        //Panda以外はGameManagerをタグから検索するように
+        if (!IsPanda)
+        {
+            gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        }
 
         // ルームに入室できたら、PhotonObject(本記事ではSphere)を生成する
         if (IsBird)
         {
             player = PhotonNetwork.Instantiate("Player/BirdPlayer", new Vector3(0f, 0f, 0f), Quaternion.identity);
-            gameManager.GetComponent<GameManager>().SetIncrementAliveCount();
+            gameManager.SetIncrementAliveCount();
         }
         else if(IsRabbit)
         {
             player = PhotonNetwork.Instantiate("Player/RabbitPlayer", new Vector3(0f, 0f, 0f), Quaternion.identity);
-            gameManager.GetComponent<GameManager>().SetIncrementAliveCount();
+            gameManager.SetIncrementAliveCount();
         }
         else if(IsMouse)
         {
             player = PhotonNetwork.Instantiate("Player/MousePlayer", new Vector3(0f, 0f, 0f), Quaternion.identity);
-            gameManager.GetComponent<GameManager>().SetIncrementAliveCount();
+            gameManager.SetIncrementAliveCount();
         }
         else if(IsPanda)
         {
             player = PhotonNetwork.Instantiate("Player/PandaPlayer", new Vector3(0f, 0f, 0f), Quaternion.identity);
-            PhotonNetwork.Instantiate(gameManager.name, new Vector3(0f, 0f, 0f), Quaternion.identity);
+            PhotonNetwork.Instantiate("GameManager", new Vector3(0f, 0f, 0f), Quaternion.identity);
         }
 
         GameObject camera = Instantiate(CameraRig, new Vector3(0f, 0f, 0f), Quaternion.identity);
