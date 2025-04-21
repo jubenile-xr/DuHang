0fbba7ca0a05c419e9745ae1ed2a66936f06362d
bearl <125728897+bearl27@users.noreply.github.com>
2025-04-21 16:07:25 +0900

    何選んでるのかわかりやすい

--- Pandator/Assets/SelectScenes/Scripts/AnimalSelectManager.cs
+++ Pandator/Assets/SelectScenes/Scripts/AnimalSelectManager.cs
@@ -5,6 +5,7 @@ public class AnimalSelectManager : MonoBehaviour
 {
     private bool isAnimalSelected = false; // 動物が選択されたかどうかのフラグ
     [SerializeField] private bool isDebugFlag = false; // 動物選択UI
+    [SerializeField] private SelectedAnimalUI selectedAnimalUI; // 動物選択UI
 
     // Update is called once per frame
     private void Update()
@@ -13,32 +14,29 @@ public class AnimalSelectManager : MonoBehaviour
         if (OVRInput.Get(OVRInput.Button.One) || Input.GetKeyDown(KeyCode.R)) // Aボタン
         {
             SelectAnimal(Character.GameCharacters.RABBIT);
+            selectedAnimalUI.RabbitedSelected();
         }
         // Bボタンが押された場合
         else if (OVRInput.Get(OVRInput.Button.Two) || Input.GetKeyDown(KeyCode.B)) // Bボタン
         {
             SelectAnimal(Character.GameCharacters.BIRD);
+            selectedAnimalUI.BirdSelected();
         }
         // Xボタンが押された場合
         else if (OVRInput.Get(OVRInput.Button.Three) || Input.GetKeyDown(KeyCode.M)) // Xボタン
         {
             SelectAnimal(Character.GameCharacters.MOUSE);
+            selectedAnimalUI.MouseSelected();
         }
         // Yボタンが押された場合
         else if (isDebugFlag && (OVRInput.Get(OVRInput.Button.Four) || Input.GetKeyDown(KeyCode.P))) // Yボタン
         {
             SelectAnimal(Character.GameCharacters.PANDA);
+            selectedAnimalUI.ResetAnimalUI();
         }
         if (isAnimalSelected && (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKeyDown(KeyCode.O)))
         {
-            if (Character.GetSelectedAnimal() == Character.GameCharacters.GOD)
-            {
-                GoToGodScene();
-            }
-            else
-            {
-                GoToTutorialScene();
-            }
+            GoToTutorialScene();
         }
     }
 
