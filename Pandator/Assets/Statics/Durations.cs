9e7d3207847fab0d75710039568517528d1f606a
bearl <125728897+bearl27@users.noreply.github.com>
2025-05-07 17:27:46 +0900

    durationをstaticで指定

---
+++ Pandator/Assets/Statics/Durations.cs
@@ -0,0 +1,7 @@
+public static class Durations
+{
+    public const float NET_GUN_DURATION = 5.0f;
+    public const float INTERRUPTED_GUN_DURATION = 5.0f;
+    // TODO: 幽霊時の弾の持続時間を調整する
+    public const float GHOST_INTERRUPTED_GUN_DURATION = 7.5f;
+}
