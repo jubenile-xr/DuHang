dd004efd4b0131ab88dfce114b072f798547e495
Kumazawa <is0667fk@ed.ritsumei.ac.jp>
2025-05-17 18:40:26 +0900

    fix

--- Pandator/Assets/Mouse/Scripts/MouseMove.cs
+++ Pandator/Assets/Mouse/Scripts/MouseMove.cs
@@ -20,9 +20,12 @@ public class MouseMove : MonoBehaviour
     [SerializeField] private float speedThreshold = 0.1f; // これより遅かったら動かない
     private InitializeManager InitializeManager;
     private float floarValue;
+    
+    private bool isGrounded = false; // 地面に触れているか
+    [SerializeField] private float fallSpeed = 1.0f; // 手動落下速度
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
         moveSpeed = normalSpeed; // 初期値を通常速度に設定
         InitializeManager = GameObject.FindWithTag("InitializeManager").GetComponent<InitializeManager>();
         floarValue = InitializeManager.GetLocalAnchorPosition().y;
@@ -77,6 +80,12 @@ public class MouseMove : MonoBehaviour
             {
                 transform.Translate(forwardDirection * totalSpeed * Time.deltaTime, Space.World);
             }
+            
+            // 地面にも壁にも触れていなければ落下させる
+            if (!isGrounded && !isCollisionWall)
+            {
+                transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+            }
         }
         
         //落ちた時用
@@ -106,7 +115,11 @@ public class MouseMove : MonoBehaviour
         if (collision.gameObject.tag == "Wall")
         {
             isCollisionWall = true;
-            rb.useGravity = false;
+            //rb.useGravity = false;
+        }
+        else if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
 
@@ -115,7 +128,11 @@ public class MouseMove : MonoBehaviour
         if (collision.gameObject.tag == "Wall")
         {
             rb.useGravity = true;
-            isCollisionWall = false;
+            //isCollisionWall = false;
+        }
+        else if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
         }
     }
 }
